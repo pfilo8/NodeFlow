7529453c4e701f7e7afb6a8c48b261cfd391d6aa
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2021-11-16 15:01:01 +0100

    Bugfix for MSD dataset.

--- src/probabilistic_flow_boosting/extras/datasets/uci_dataset.py
+++ src/probabilistic_flow_boosting/extras/datasets/uci_dataset.py
@@ -8,7 +8,8 @@ from kedro.io import AbstractDataSet
 
 class UCIDataSet(AbstractDataSet):
 
-    def __init__(self, filepath_data: str, filepath_index_columns: str, filepath_index_rows: str):
+    def __init__(self, filepath_data: str, filepath_index_columns: str, filepath_index_rows: str,
+                 load_args: Dict[str, Any] = None):
         """Creates a new instance of UCIDataSet to load / save image data at the given filepath.
 
         Args:
@@ -19,13 +20,16 @@ class UCIDataSet(AbstractDataSet):
         self._filepath_data = filepath_data
         self._filepath_index_columns = filepath_index_columns
         self._filepath_index_rows = filepath_index_rows
+        self._load_args = load_args
 
     def _load(self) -> np.ndarray:
-        data = np.loadtxt(self._filepath_data)
+        data = np.loadtxt(self._filepath_data, delimiter=self._load_args.get("data_delimiter"))
         data = pd.DataFrame(data)
-        index_columns = np.loadtxt(self._filepath_index_columns, dtype=np.int32)
+        index_columns = np.loadtxt(self._filepath_index_columns, dtype=np.int32,
+                                   delimiter=self._load_args.get("index_columns_delimiter"))
         index_columns = index_columns.reshape(-1)  # Issues when loading target column
-        index_rows = np.loadtxt(self._filepath_index_rows, dtype=np.int32)
+        index_rows = np.loadtxt(self._filepath_index_rows, dtype=np.int32,
+                                delimiter=self._load_args.get("index_rows_delimiter"))
         return data.iloc[index_rows, index_columns]
 
     def _save(self, data: np.ndarray) -> None:
